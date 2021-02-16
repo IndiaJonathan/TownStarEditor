d74dd1ef66ec62e5a785ea6c9fd4bf486a0fbfa3
Jonathan <IndiaJonathan@gmail.com>
2021-02-16 14:01:52 -0600

    Create CraftItem.vue

---
+++ client/src/components/CraftItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      selected: ''
+    }
+  },
+  watch: {
+    selected (selection) {
+      console.log('Selection has changed', selection)
+    }
+  },
+  props: {
+    CityPoints: {
+      type: Number,
+      required: true
+    },
+    Name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
